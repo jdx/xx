02951f5f3409ee9cbb05142849b5c9b0691c66aa
Jeff Dickey <216188+jdx@users.noreply.github.com>
2024-02-08 16:26:52 -0600

    added regex

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,7 @@ pub use error::{XXError, XXResult};
 pub mod context;
 pub mod error;
 pub mod file;
+mod regex;
 
 #[cfg(test)]
 pub mod test;
---
+++ src/regex.rs
@@ -0,0 +1,17 @@
+#[macro_export]
+macro_rules! regex {
+    ($re:literal $(,)?) => {{
+        static RE: std::sync::OnceLock<regex::Regex> = std::sync::OnceLock::new();
+        RE.get_or_init(|| regex::Regex::new($re).unwrap())
+    }};
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_regex() {
+        let re = regex!("^\\d{4}-\\d{2}-\\d{2}$");
+        assert!(re.is_match("2021-01-01"));
+        assert!(!re.is_match("2021-01-01-01"));
+    }
+}
