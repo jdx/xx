f144cb886a18e3d4f76fc21c8c0279dba6262e25
Jeff Dickey <216188+jdx@users.noreply.github.com>
2024-02-09 23:02:29 -0600

    wip

--- src/error.rs
+++ src/error.rs
@@ -6,11 +6,11 @@ use thiserror::Error;
 #[derive(Error, Diagnostic, Debug)]
 pub enum XXError {
     #[error("{0}\nFile: {1}")]
-    #[diagnostic(code(xxerr::file), url(docsrs))]
+    #[diagnostic(code(xx::file), url(docsrs))]
     FileError(std::io::Error, PathBuf),
 
     #[error("{0}\n{1}")]
-    #[diagnostic(code(xxerr::process), url(docsrs))]
+    #[diagnostic(code(xx::process), url(docsrs))]
     ProcessError(std::io::Error, String),
 }
 
