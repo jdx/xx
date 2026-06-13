b2c95fc9afbd46b6871397e1f9db7b33a6e7d8a3
jdx <216188+jdx@users.noreply.github.com>
2026-06-13 04:56:34 -1000

    fix: support wasm32-wasip1 builds (#243)

--- src/home.rs
+++ src/home.rs
@@ -1,11 +1,11 @@
 use std::path::PathBuf;
 
-#[cfg(not(all(target_family = "wasm", target_os = "unknown")))]
+#[cfg(not(target_family = "wasm"))]
 pub(crate) fn home_dir() -> Option<PathBuf> {
     homedir::my_home().ok().flatten()
 }
 
-#[cfg(all(target_family = "wasm", target_os = "unknown"))]
+#[cfg(target_family = "wasm")]
 pub(crate) fn home_dir() -> Option<PathBuf> {
     None
 }
